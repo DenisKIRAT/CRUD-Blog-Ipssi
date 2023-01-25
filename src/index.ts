0e20cb92656312132cae0e3f4cba6eb06062355d
Steven <sivakaran19@hotmail.fr>
2023-01-25 14:29:41 +0100

    add index.ts (base)

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+// import userRoutes from './routes/user'
+// import todoListRoutes from './routes/todoList'
+// import todoItemRoutes from './routes/todoItem'
+// import { protect } from './modules/auth'
+// import { createNewUser, signIn } from './handlers/user'
+// import config from './config'
+
+dotenv.config()
+
+const app = express()
+// const PORT = config.port
+
+const PORT = 1234
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Salut' })
+})
+
+// app.use('/api', protect, [
+//   userRoutes,
+//   todoListRoutes,
+//   todoItemRoutes
+// ])
+
+// app.post('/signUp', createNewUser)
+// app.post('/signIn', signIn)
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+})
