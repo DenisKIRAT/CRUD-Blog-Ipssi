d4e5f33fbaace759450a8f3dbbe9eacf5ccddade
DenisKIRAT <denis.kirat@gmail.com>
2023-01-26 09:54:13 +0100

    add cascade delete on post and comment models

--- src/routes/comments.ts
+++ src/routes/comments.ts
@@ -1,10 +1,54 @@
 import express from 'express'
+import { body, validationResult } from 'express-validator'
+import db from '../db'
 
 const app = express.Router()
 
-app.get('/comments', (req, res) => {
-//   console.log(req.posts)
-  res.status(200).json({ message: 'Hello comments' })
-})
+// app.get('/comments', (req, res) => {
+// //   console.log(req.posts)
+//   res.status(200).json({ message: 'Hello comments' })
+// })
+
+// app.get(
+//   '/comments',
+//   async (req, res) => {
+//       try {
+//           const post = await db.comment.findMany({
+//           where: {
+//               authorId: req.user.id
+//           },
+//           include: {
+//               post: true
+//           }
+//           })
+  
+//           return res.status(200).json(post)
+//       } catch(e) {
+//           return res.status(400).json({ message: 'Not found' })
+//       }
+//   }
+// )
+
+app.post(
+  '/comment',
+  body('postId').isUUID().notEmpty(),
+  body('content').isString().notEmpty(),
+  async (req, res) => {
+    try {
+      validationResult(req).throw()
+      const createdContent  = await db.comment.create({
+        data: {
+          content: req.body.content,
+          postId: req.body.postId,
+          authorId: req.user.id
+        },
+      })
+
+      return res.status(201).json(createdContent)
+    } catch (e) {
+      return res.status(400).json({ message: e || 'Error during creation'})
+    }
+  }
+)
 
 export default app
